2e0a2c238dda204652eff79360dfc1eca725d7f5
Nguweneza P Christian <nguwenezacc982@gmail.com>
2024-05-10 23:57:52 +0200

    divide the total_score by the total number of corrections

--- 0x00-MySQL_Advanced/7-average_score.sql
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -7,18 +7,18 @@ CREATE PROCEDURE ComputeAverageScoreForUser(
 )
 BEGIN
     DECLARE total_score FLOAT;
-    DECLARE total_projects INT;
+    DECLARE total_corrections INT;
     DECLARE avg_score FLOAT;
 
     -- Compute total score for the user
     SELECT SUM(score) INTO total_score FROM corrections WHERE user_id = user_id;
 
-    -- Compute total number of projects for the user
-    SELECT COUNT(DISTINCT project_id) INTO total_projects FROM corrections WHERE user_id = user_id;
+    -- Compute total number of corrections for the user
+    SELECT COUNT(*) INTO total_corrections FROM corrections WHERE user_id = user_id;
 
     -- Compute average score
-    IF total_projects > 0 THEN
-        SET avg_score = total_score / total_projects;
+    IF total_corrections > 0 THEN
+        SET avg_score = total_score / total_corrections;
     ELSE
         SET avg_score = 0;
     END IF;
