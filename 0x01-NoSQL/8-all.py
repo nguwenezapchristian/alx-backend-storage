46213a5f5221ba91ec82db6af5cbf3db30ceed5c
Nguweneza P Christian <nguwenezacc982@gmail.com>
2024-05-15 01:37:04 +0200

    task 7 and 8

---
+++ 0x01-NoSQL/8-all.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+Python function that lists all documents in a collection
+"""
+import pymongo
+
+
+def list_all(mongo_collection):
+    """ Lists all elements in a collection """
+    if not mongo_collection:
+        return []
+    return list(mongo_collection.find())
