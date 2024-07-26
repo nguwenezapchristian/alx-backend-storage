63ac2afe87cbe24164629d44488388cbf43b1bbd
nguwenezapchristian <nguwenezacc982@gmail.com>
2024-07-26 11:00:29 +0200

    adding the top 10 of the most present IPs in the collection nginx

--- 0x01-NoSQL/102-log_stats.py
+++ 0x01-NoSQL/102-log_stats.py
@@ -1,44 +1,40 @@
 #!/usr/bin/env python3
 """ 102-log_stats """
-
 from pymongo import MongoClient
 
 
-def print_log_stats():
-    """ Prints statistics about Nginx logs from MongoDB with top IPs """
-    client = MongoClient('mongodb://localhost:27017')
+def log_stats():
+    """ Connect to MongoDB """
+    client = MongoClient('mongodb://127.0.0.1:27017')
     db = client.logs
     collection = db.nginx
 
-    # Total number of logs
-    total_logs = collection.count_documents({})
+    log_count = collection.count_documents({})
+    print(f"{log_count} logs")
 
-    # Methods counts
     methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
-    method_counts = {method: collection.count_documents(
-        {"method": method}) for method in methods}
+    print("Methods:")
 
-    # Logs with method=GET and path=/status
-    status_check = collection.count_documents(
+    for method in methods:
+        method_count = collection.count_documents({"method": method})
+        print(f"\tmethod {method}: {method_count}")
+
+    status_check_count = collection.count_documents(
         {"method": "GET", "path": "/status"})
+    print(f"{status_check_count} status check")
 
-    # Top 10 IPs
-    ip_counts = collection.aggregate([
+    pipeline = [
         {"$group": {"_id": "$ip", "count": {"$sum": 1}}},
         {"$sort": {"count": -1}},
         {"$limit": 10}
-    ])
+    ]
+    top_ips = list(collection.aggregate(pipeline))
 
-    # Print results
-    print(f"{total_logs} logs")
-    print("Methods:")
-    for method in methods:
-        print(f"\tmethod {method}: {method_counts[method]}")
-    print(f"{status_check} status check")
     print("IPs:")
-    for ip in ip_counts:
+    for ip in top_ips:
         print(f"\t{ip['_id']}: {ip['count']}")
 
 
 if __name__ == "__main__":
-    print_log_stats()
+    """ Main Function """
+    log_stats()
