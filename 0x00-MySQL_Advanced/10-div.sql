0f1f4d06f1bacfb330101ad202d27a89ea615e50
nguwenezapchristian <nguwenezacc982@gmail.com>
2024-07-26 12:07:35 +0200

    script that creates a function SafeDiv

--- 0x00-MySQL_Advanced/10-div.sql
+++ 0x00-MySQL_Advanced/10-div.sql
@@ -2,13 +2,9 @@
 CREATE FUNCTION SafeDiv(a INT, b INT)
 RETURNS INT
 BEGIN
-    DECLARE result INT;
-    
     IF b <> 0 THEN
-        SET result = a / b;
+        RETURN a / b;
     ELSE
-        SET result = 0;
+        RETURN 0;
     END IF;
-    
-    RETURN result;
 END;
